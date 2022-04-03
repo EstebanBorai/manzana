2cf12383372790c9c61cd6305182ff3dbebb4e0b
Esteban Borai <estebanborai@gmail.com>
2022-04-02 23:42:37 -0400

    feat: ci workflow runs on yml only

--- bin/make-wrangler.sh
+++ bin/make-wrangler.sh
@@ -7,4 +7,4 @@
 
 WORKER_NAME="home"
 
-echo -e "type = 'javascript'\nname = 'manzana'\nroute = 'manzana.estebanborai.com/*'\ncompatibility_date = '2022-04-02'\naccount_id = '$CF_ACCOUNT_ID'\nworkers_dev = false\nzone_id = '$CF_ZONE_ID'\n\n[site]\nbucket = './docs/build'\nentry-point = 'workers-site'\n\n[build]\ncommand = 'npm run build:docs'\nwatch_dir = 'app'\n\n[build.upload]\nformat = 'service-worker'" > wrangler.toml
+echo -e "type = 'javascript'\nname = 'manzana'\nroute = 'manzana.estebanborai.com/*'\ncompatibility_date = '2022-04-02'\naccount_id = '$CF_ACCOUNT_ID'\nworkers_dev = false\nzone_id = '$CF_ZONE_ID'\n\n[site]\nbucket = './build'\nentry-point = 'workers-site'\n\n[build]\ncommand = 'npm run build:docs'\nwatch_dir = 'app'\n\n[build.upload]\nformat = 'service-worker'" > wrangler.toml
